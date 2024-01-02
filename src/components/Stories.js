66a0a3e3f8980a0ed14ad2304fb2aa1752112a7a
Hembunos <hembunospro1428@gmail.com>
2024-01-02 23:46:12 +0530

    modified

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import Shimmer from './Shimmer';
+import { useContext } from 'react';
+import { AppContext, useGlobalContext } from '../context/context';
 
 
 
 const Stories = () => {
 
-  let isLoading = true;
 
+  const data = useGlobalContext();
 
+  let isLoading = false;
   if (isLoading) {
     return (
       <Shimmer/>
     )
   }
 
-
-
   return (
     <>
       <h2>Stories</h2>
+      <h1>{ data }</h1>
     </>
   )
 }
